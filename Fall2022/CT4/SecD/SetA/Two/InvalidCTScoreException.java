435c2449f9f1b9b72e84b35e413f062cb72ce883
Sharif Yousuf <sharifmdyousuf007@gmail.com>
2023-10-03 21:38:22 +0600

    z

--- Fall2022/CT4/SecD/SetA/Two/InvalidCTScoreException.java
+++ Fall2022/CT4/SecD/SetA/Two/InvalidCTScoreException.java
@@ -1,7 +1,10 @@
 package Fall2022.CT4.SecD.SetA.Two;
 
+// Define a custom exception class named InvalidCTScoreException that extends the built-in Exception class.
 public class InvalidCTScoreException extends Exception {
+    // Create a constructor that takes the cutoff CT score as a parameter.
     public InvalidCTScoreException(int mark) {
+        // Call the constructor of the parent Exception class and set the exception message.
         super("You need a minimum score of " + mark + " in CT to attend the final exam.");
     }
 }
