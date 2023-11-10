14669c9ee66345be84140249ee34fbba8f374ad4
Sharif Yousuf <sharifmdyousuf007@gmail.com>
2023-11-10 19:15:42 +0600

    z

--- Fall2022/Final/Two/B.java
+++ Fall2022/Final/Two/B.java
@@ -8,7 +8,7 @@ public class B {
             return false;
         }
 
-        for (int i = 2; i < n / 2; i++) {
+        for (int i = 2; i <= n / 2; i++) {
             if (n % i == 0) {
                 return false;
             }
