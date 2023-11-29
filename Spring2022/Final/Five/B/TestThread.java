24377661b322bc5009a6bf65780e2cffe3f04d67
Sharif Yousuf <sharifmdyousuf007@gmail.com>
2023-11-30 05:14:02 +0600

    Refactor exception handling in TestThread.java

--- Spring2022/Final/Five/B/TestThread.java
+++ Spring2022/Final/Five/B/TestThread.java
@@ -1,19 +1,15 @@
 package Spring2022.Final.Five.B;
 
-import java.io.FileReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 
 public class TestThread {
     public static void main(String[] args) {
         try {
             new FileReader("test.txt").read();
         } catch (FileNotFoundException e) {
-            // Handle FileNotFoundException here
-            System.out.println("File not found: " + e.getMessage());
+            e.printStackTrace();
         } catch (IOException e) {
-            // Handle other IOException here
-            System.out.println("IOException: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
