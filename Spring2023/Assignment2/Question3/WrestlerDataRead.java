9e8ec5a577278efc83e3ff774269433d12d30dfe
Sharif Yousuf <sharifmdyousuf007@gmail.com>
2023-11-30 03:29:32 +0600

    Refactor wrestler data reading logic

--- Spring2023/Assignment2/Question3/WrestlerDataRead.java
+++ Spring2023/Assignment2/Question3/WrestlerDataRead.java
@@ -1,35 +1,25 @@
 package Spring2023.Assignment2.Question3;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.io.*;
 
 public class WrestlerDataRead {
     public static void main(String[] args) {
-        ArrayList<Wrestler> wrestlers = new ArrayList<>();
 
-        try (BufferedReader reader = new BufferedReader(new FileReader("D:\\Programming\\VSCode\\CSE 203 CT Solve\\Spring2023\\Assignment2\\Question3\\data.txt"))) {
-            String line;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("D:\\Programming\\VSCode\\CSE 203 CT Solve\\Spring2023\\Assignment2\\Question3\\data.txt"));
+            
+            String[] w1Data = br.readLine().split(", ");
+            String[] w2Data = br.readLine().split(", ");
 
-            while ((line = reader.readLine()) != null) {
-                String[] parts = line.split(", ");
+            Wrestler w3 = new Wrestler(w1Data[0], Integer.parseInt(w1Data[1]), w1Data[2]);
+            Wrestler w4 = new Wrestler(w2Data[0], Integer.parseInt(w2Data[1]), w2Data[2]);
 
-                if (parts.length == 3) {
-                    String name = parts[0];
-                    int age = Integer.parseInt(parts[1]);
-                    String finishingMove = parts[2];
+            System.out.println(w3);
+            System.out.println(w4);
 
-                    Wrestler wrestler = new Wrestler(name, age, finishingMove);
-                    wrestlers.add(wrestler);
-                }
-            }
+            br.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
-        }
-
-        for (Wrestler wrestler : wrestlers) {
-            System.out.println(wrestler);
         }
     }
 }
