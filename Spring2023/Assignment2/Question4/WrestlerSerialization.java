587ee0553e87dfef22268760079f88d8fe8979f3
Sharif Yousuf <sharifmdyousuf007@gmail.com>
2023-11-30 03:51:34 +0600

    Refactor WrestlerSerialization class to serialize individual Wrestler objects

--- Spring2023/Assignment2/Question4/WrestlerSerialization.java
+++ Spring2023/Assignment2/Question4/WrestlerSerialization.java
@@ -1,32 +1,34 @@
 package Spring2023.Assignment2.Question4;
 
 import java.io.*;
-import java.util.ArrayList;
 
 public class WrestlerSerialization {
     public static void main(String[] args) {
-        ArrayList<Wrestler> wrestlers = new ArrayList<>();
-        wrestlers.add(new Wrestler("John Cena", 46, "Attitude Adjustment"));
-        wrestlers.add(new Wrestler("The Rock", 51, "Rock Bottom"));
+        Wrestler w1 = new Wrestler("John Cena", 46, "Attitude Adjustment");
+        Wrestler w2 = new Wrestler("The Rock", 51, "Rock Bottom");
 
         // Serializing Wrestler objects and saving to data_ser.txt
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("D:\\Programming\\VSCode\\CSE 203 CT Solve\\Spring2023\\Assignment2\\Question4\\data_ser.txt"))) {
-            oos.writeObject(wrestlers);
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("D:\\Programming\\VSCode\\CSE 203 CT Solve\\Spring2023\\Assignment2\\Question4\\data_ser.txt"));
+            
+            oos.writeObject(w1);
+            oos.writeObject(w2);
+
+            oos.close();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
 
         // Deserializing Wrestler objects from data_ser.txt
-        ArrayList<Wrestler> deserializedWrestlers = new ArrayList<>();
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream("D:\\Programming\\JetBrains\\CSE203Assignment2\\src\\Question4\\data_ser.txt"));
+            
+            Wrestler w3 = (Wrestler) ois.readObject();
+            Wrestler w4 = (Wrestler) ois.readObject();
 
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("D:\\Programming\\JetBrains\\CSE203Assignment2\\src\\Question4\\data_ser.txt"))) {
-            deserializedWrestlers = (ArrayList<Wrestler>) ois.readObject();
+            ois.close();
         } catch (IOException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-
-        for (Wrestler wrestler : deserializedWrestlers) {
-            System.out.println(wrestler);
+            e.printStackTrace();
         }
     }
 }
