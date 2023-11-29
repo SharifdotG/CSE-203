a7ac1526ecbba62468ae60c3bb3256ae834552de
Sharif Yousuf <sharifmdyousuf007@gmail.com>
2023-11-30 03:20:14 +0600

    Refactor wrestler data writing logic

--- Spring2023/Assignment2/Question2/WrestlerData.java
+++ Spring2023/Assignment2/Question2/WrestlerData.java
@@ -1,20 +1,19 @@
 package Spring2023.Assignment2.Question2;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.io.*;
 
 public class WrestlerData {
     public static void main(String[] args) {
-        ArrayList<Wrestler> wrestlers = new ArrayList<>();
-        wrestlers.add(new Wrestler("John Cena", 46, "Attitude Adjustment"));
-        wrestlers.add(new Wrestler("The Rock", 51, "Rock Bottom"));
+        Wrestler w1 = new Wrestler("John Cena", 46, "Attitude Adjustment");
+        Wrestler w2 = new Wrestler("The Rock", 51, "Rock Bottom");
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("D:\\Programming\\VSCode\\CSE 203 CT Solve\\Spring2023\\Assignment2\\Question2\\data.txt"))) {
-            for (Wrestler wrestler : wrestlers) {
-                writer.write(wrestler.toString() + "\n");
-            }
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter("D:\\Programming\\VSCode\\CSE 203 CT Solve\\Spring2023\\Assignment2\\Question2\\data.txt"));
+            
+            bw.write(w1 + "\n");
+            bw.write(w2 + "\n");
+
+            bw.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
